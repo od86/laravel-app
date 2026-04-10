8856792bba8475a05d44904a355033c093959b25
od86 <178620777+od86@users.noreply.github.com>
2026-04-10 10:24:33 +0100

    Added routes for posts.new

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/posts/new', [PostController::class, 'create']);
+Route::post('/posts/new', [PostController::class, 'store']);
