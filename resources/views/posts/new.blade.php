04cc7bafe371a631faa0caa445a3ea96aebc71e8
od86 <178620777+od86@users.noreply.github.com>
2026-04-10 10:23:30 +0100

    Created posts.new page

---
+++ resources/views/posts/new.blade.php
@@ -0,0 +1,18 @@
+<x-layout>
+  <h1>New Page</h1>
+
+  <form action="/posts/new" method="post">
+    <div class="field">
+      <label for="title">Title</label>
+      <input type="text" id="title" name="title">
+    </div>
+
+    <div class="field">
+      <textarea name="body" id="body" placeholder="Today I went to..."></textarea>
+    </div>
+
+    <div class="actions">
+      <button type="submit">Submit</button>
+    </div>
+  </form>
+</x-layout>
