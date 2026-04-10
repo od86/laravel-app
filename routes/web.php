9314d83f9993866f73b065fc5755ae4ec09f60d4
od86 <178620777+od86@users.noreply.github.com>
2026-04-10 10:25:06 +0100

    Added route for posts.index

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,6 @@ use App\Http\Controllers\PostController;
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/posts/index', [PostController::class, 'index']);
 Route::get('/posts/new', [PostController::class, 'create']);
 Route::post('/posts/new', [PostController::class, 'store']);
